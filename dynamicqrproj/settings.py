2850e571d6f63d03f202a0a4a54945e4efbb32ea
Abhirath Dubey <abhirathdubey2804@gmail.com>
2023-04-26 18:42:15 +0530

    vercel changes

--- dynamicqrproj/settings.py
+++ dynamicqrproj/settings.py
@@ -112,11 +112,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
