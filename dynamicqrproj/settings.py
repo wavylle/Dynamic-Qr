22b92266ad69638db354226bab9869437642e9c2
Abhirath Dubey <abhirathdubey2804@gmail.com>
2023-04-26 20:48:59 +0530

    sett

--- dynamicqrproj/settings.py
+++ dynamicqrproj/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
